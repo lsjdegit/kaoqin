08e55e828aa105c0b3b4c4c3f067cf97ac49d741
lsjdegit <2807728147@qq.com>
2020-05-28 13:44:33 +0800

    Initial commit

---
+++ src/com/param/TimeParam.java
@@ -0,0 +1,22 @@
+package com.param;
+
+public class TimeParam {
+    private String onTime = "8:30";
+    private String offTime = "17:30";
+
+    public String getOnTime() {
+        return onTime;
+    }
+
+    public void setOnTime(String onTime) {
+        this.onTime = onTime;
+    }
+
+    public String getOffTime() {
+        return offTime;
+    }
+
+    public void setOffTime(String offTime) {
+        this.offTime = offTime;
+    }
+}
